a85a262e16bbf88ce2be1825ac1685e2b7ccd4c4
Divine <dpkreativ@gmail.com>
2021-03-23 08:30:33 +0100

    done with the challenge

--- katas/8kyu/DNAtoRNA.js
+++ katas/8kyu/DNAtoRNA.js
@@ -12,6 +12,8 @@ For example:
 The input string can be of arbitrary length - in particular, it may be empty. All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
 */
 
-const DNAtoRNA = (dna) => {};
+const DNAtoRNA = (dna) => {
+  return dna.replace(/U/gi, "T");
+};
 
 export default DNAtoRNA;
