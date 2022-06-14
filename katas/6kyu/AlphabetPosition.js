96a251f7d3b7f338de7b58c3d6fc2aa2bb649f00
Divine Orji <dpkreativ@gmail.com>
2022-06-14 23:05:23 +0100

    Write starter function for new codewars challenge

---
+++ katas/6kyu/AlphabetPosition.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/546f922b54af40e1e90001da/train/javascript
+const test = "The sunset sets at twelve o' clock.";
+
+function alphabetPosition(text) {
+  return text;
+}
+
+console.log(alphabetPosition);
